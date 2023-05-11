ea9f97f1e01d82f09fcd04af5aa1ec6ba1636904
Smriti225 <smritikimtee611@gmail.com>
2023-05-11 16:59:40 +0530

    verification code changes

--- project/project/authapp/models.py
+++ project/project/authapp/models.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from django.contrib.auth.models import User
 
 class verification_code(models.Model):
-    email=models.ForeignKey(User,on_delete=models.CASCADE)
+    email=models.EmailField()
     code=models.IntegerField()
     created_on=models.DateTimeField(default=datetime.now())
     expiry_on=models.DateTimeField(default=datetime.now() + timedelta(minutes=3))
